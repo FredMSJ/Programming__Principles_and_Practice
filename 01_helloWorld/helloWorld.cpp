bbcab32b27bc4b786117ec310f7c28cbf6c69fe0
FredMSJ <fmdsj13@gmail.com>
2021-04-06 10:22:59 -0300

    Comments

--- 01_helloWorld/helloWorld.cpp
+++ 01_helloWorld/helloWorld.cpp
@@ -1,8 +1,8 @@
 #include "../std_lib_facilities.h"
 
-int main()
+int main() //C++ Programs start by executing the function main.
 {
-  cout << "Hello, World!\n";
-  keep_window_open();
+  cout << "Hello, World!\n"; //Output "Hello, World!".
+  keep_window_open();        //wait for a character to be entered.
   return 0;
 }
