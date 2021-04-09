05ea25ade75998b350a65eb38d136294fdb5d914
FredMDSJ <fmdsj13@gmail.com>
2021-04-09 17:33:33 -0300

    second exercise done

---
+++ 02_helloWorld/Exercises/02_second.cpp
@@ -0,0 +1,12 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "The guide to bathroom.\n";
+  cout << "01. Open the house door\n02. Go ahead to the dining room.\n";
+  cout << "03. Turn left once.\n04. Go ahead and turn left again.\n";
+  cout << "05. Turn left again.\n06. Open the door.\n";
+  cout << "07. Feel free to use bathroom.\n";
+  keep_window_open();
+  return 0;
+}
