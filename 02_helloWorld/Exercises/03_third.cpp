4ca22d6e74e3565d322f918ac43b245be5f031e8
FredMSJ <fmdsj13@gmail.com>
2021-04-10 17:48:11 -0300

    doing the third ecercise

---
+++ 02_helloWorld/Exercises/03_third.cpp
@@ -0,0 +1,12 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "01. When you stand front the door, open it.\n";
+  cout << "02. After open the door, turn left for the stair.\n";
+  cout << "03. When you stand front the stairs, walk till the end of it.\n";
+  cout << "04. Turn 90 left twice.\n";
+  cout << "05. Walk ahead and open the doors until you see the world outside.\n";
+  keep_window_open();
+  return 0;
+}
