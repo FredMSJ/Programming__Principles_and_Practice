19d06c964f19b56fe65bace7594f8583a4b34ec1
FredMDSJ <fmdsj13@gmail.com>
2021-04-14 17:09:31 -0300

    first try this resolved

---
+++ 03_ Objects_Types_and_Values/pag95/Try_This.cpp
@@ -0,0 +1,15 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "Please insert your name and your age(press enter):\n";
+  string name;
+  double age;
+  cin >> name >> age;
+  double ageInMonths = age * 12;
+
+  cout << name << ", your age in months is: " << ageInMonths << '\n';
+  keep_window_open();
+
+  return 0;
+}
