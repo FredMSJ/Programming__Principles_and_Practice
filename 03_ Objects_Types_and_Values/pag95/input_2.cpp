410cd914b70b3bde558d59b75bb91366f9f6d2c4
FredMDSJ <fmdsj13@gmail.com>
2021-04-14 16:52:53 -0300

    pag95 input_2

---
+++ 03_ Objects_Types_and_Values/pag95/input_2.cpp
@@ -0,0 +1,12 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "Please enter your first and second names\n";
+  string first;
+  string second;
+  cin >> first >> second;
+  cout << "Hello," << first << " " << second << '\n';
+  keep_window_open();
+  return 0;
+}
