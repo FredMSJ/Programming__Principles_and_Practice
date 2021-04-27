0959ddbd0ea01e57727271b835cc629e7e7a9a24
FredMDSJ <fmdsj13@gmail.com>
2021-04-27 19:00:58 -0300

    complete when in home

---
+++ 03_ Objects_Types_and_Values/pag97/floating-point_numbers.cpp
@@ -0,0 +1,10 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "Please enter a floating-point value: ";
+  double n;
+  cin >> n;
+  cout << "n == " << n
+       << "\nn+1 == " << n + 1 //complete when in home.
+}
