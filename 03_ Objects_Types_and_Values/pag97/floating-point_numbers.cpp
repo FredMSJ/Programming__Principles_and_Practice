8975d2ae232379486917ff27b853305a5dff6419
FredMSJ <fmdsj13@gmail.com>
2021-04-28 02:55:03 -0300

    completed in home. Gotta go fast

--- 03_ Objects_Types_and_Values/pag97/floating-point_numbers.cpp
+++ 03_ Objects_Types_and_Values/pag97/floating-point_numbers.cpp
@@ -6,5 +6,12 @@ int main()
   double n;
   cin >> n;
   cout << "n == " << n
-       << "\nn+1 == " << n + 1 //complete when in home.
+       << "\nn+1 == " << n + 1
+       << "\nthree times n == " << 3 * n
+       << "\ntwice n == " << n + n
+       << "\nn squared == " << n * n
+       << "\nhalf of n == " << n / 2
+       << "\nsquared root of n == " << sqrt(n) << "\n";
+  keep_window_open();
+  return 0;
 }
