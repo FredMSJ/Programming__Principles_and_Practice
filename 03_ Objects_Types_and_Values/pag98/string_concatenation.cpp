264b6c10c4bb5a1adcf78f8c52e8b4c40b8d54a8
FredMSJ <fmdsj13@gmail.com>
2021-04-28 10:53:17 -0300

    working with strings

---
+++ 03_ Objects_Types_and_Values/pag98/string_concatenation.cpp
@@ -0,0 +1,24 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+  cout << "Please enter two names:\n";
+  string first;
+  string second;
+  cin >> first >> second;
+  if (first == second)
+  {
+    cout << "that's the same name twice\n";
+  }
+  if (first < second)
+  {
+    cout << first << " is alphabetically before " << second << '\n';
+  }
+  if (first > second)
+  {
+    cout << first << " is alphabetically after " << second << '\n';
+  }
+
+  keep_window_open();
+  return 0;
+}
