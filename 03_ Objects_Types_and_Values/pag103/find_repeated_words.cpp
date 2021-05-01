3d9f8fc830491285d9785456508c0cde02a58cea
FredMSJ <fmdsj13@gmail.com>
2021-05-01 20:59:47 -0300

    ...

---
+++ 03_ Objects_Types_and_Values/pag103/find_repeated_words.cpp
@@ -0,0 +1,20 @@
+#include "../../lib/std_lib_facilities.h"
+
+int main()
+{
+  int number_of_words = 0;
+  string previous = ""; //not a word
+  string current;
+  while (cin >> current)
+  {
+    ++number_of_words; //increase word count.
+    if (previous == current)
+    {
+      cout << "word number " << number_of_words
+           << " repeated: " << current << '\n';
+    }
+    previous = current; //put it out of "if" to work.
+  }
+  keep_window_open();
+  return 0;
+}
