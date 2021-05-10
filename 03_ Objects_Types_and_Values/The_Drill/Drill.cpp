f789cce0245b87079fac5ef0088d0e32cba0552a
FredMDSJ <fmdsj13@gmail.com>
2021-05-10 17:55:55 -0300

    finish in home

---
+++ 03_ Objects_Types_and_Values/The_Drill/Drill.cpp
@@ -0,0 +1,19 @@
+#include "../../lib/std_lib_facilities.h"
+
+int main()
+{
+  string fisrt_name;
+  string friend_name;
+  cout << "Enter the name of the person you want to write:\n";
+  cin >> fisrt_name;
+  cout << "Enter one friend name: \n";
+  cin >> friend_name;
+
+  //start the letter
+  cout << "Dear " << fisrt_name << ", how are you? I am fine. I miss you.\n"
+       << "Hope you're ok. Please send me updates of you, ok?\n"
+       << "Hey, have you seen " << friend_name << " miss you both! ";
+
+  keep_window_open();
+  return 0;
+}
