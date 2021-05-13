2f9a811b3fc0b2f18b6f65dccee2265543255234
FredMDSJ <fmdsj13@gmail.com>
2021-05-13 16:16:01 -0300

    removing comments

--- 03_ Objects_Types_and_Values/Exercises/10_tenth.cpp
+++ 03_ Objects_Types_and_Values/Exercises/10_tenth.cpp
@@ -6,7 +6,7 @@ int main()
   double val1 = 0;
   double val2 = 0;
   double res = 0;
-  cout << "Please enter the operation followed by two operands : \n";
+  cout << "Please enter the operation (+, -, *, /, plus, minus, mul, div) followed by two floating-point values separated by space : \n";
   while (cin >> operation >> val1 >> val2)
   {
     if (operation == "+" || operation == "plus")
@@ -35,28 +35,3 @@ int main()
   cout << "exit because of bad input.\n";
   keep_window_open("~");
 }
-/*
-string operation;
-	double val1 = 0;	// first/leftmost operand
-	double val2 = 0;	// second/rightmost operand
-	cout << "Please enter an operation (+, -, *, /, plus, minus, mul, div) followed by two floating-point values separated by a space: ";
-	
-	while(cin >> operation >> val1 >> val2) {	// we do the following as long as we get "good" input
-												// and then stop
-												// loops will be explainied in detail in Chapter 4
-		double res = 0;
-		if (operation=="plus" || operation=="+") res = val1+val2;
-		else if (operation=="minus" || operation=="-") res = val1-val2;
-		else if (operation=="mul" || operation=="*") res = val1*val2;
-		else if (operation=="div" || operation=="/") {
-			if (val2==0) error("trying to divide by zero");
-			res = val1/val2;
-		}
-		else error("sorry: bad operator: ",operation);
-
-		cout << val1 << operation << val2 << " == " << res <<'\n';
-		cout << "Please try again: ";
-	}
-	cout << "exit because of bad input\n";
-	keep_window_open("~");	// For some Windows(tm) setups
-*/
